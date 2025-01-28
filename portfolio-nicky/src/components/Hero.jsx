49f47f923c286c7fc57c4cd3aa3d0b2f9df4e00e
Alexander <nicky-nico-1998@outlook.com>
2025-01-28 13:29:02 +0000

    Hero section content and style started.

--- portfolio-nicky/src/components/Hero.jsx
+++ portfolio-nicky/src/components/Hero.jsx
@@ -1,5 +1,14 @@
 function Hero() {
-  return <></>;
+  return (
+    <div className="bg-gray-800 w-full text-center p-7 my-4">
+      <h1 className="text-white text-2xl">Hey There, I'm</h1>
+      <p className="text-white p-3 text-lg">
+        <span className="text-gray-500 font-medium">Nicky </span>
+        Alexander, A
+        <span className="text-gray-500 font-medium"> Front-End Developer</span>
+      </p>
+    </div>
+  );
 }
 
 export default Hero;
