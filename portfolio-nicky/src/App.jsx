3d247abe4701e88130d6ff16e8b025ec95595616
Alexander <nicky-nico-1998@outlook.com>
2025-01-28 13:11:47 +0000

    Hero component added

--- portfolio-nicky/src/App.jsx
+++ portfolio-nicky/src/App.jsx
@@ -2,12 +2,14 @@ import React from "react";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
 import Projects from "./components/Projects";
+import Hero from "./components/Hero";
 import "./styles/index.css";
 
 const App = () => {
   return (
     <div>
       <Header />
+      <Hero />
       <Projects />
       <Footer />
     </div>
